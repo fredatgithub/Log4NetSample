d818bea23fb868b3cb024c9ee14ab8c4808615da
fredatgithub <domaine75@gmail.com>
2021-02-14 10:50:45 +0100

    remove duplicate code

--- ConsoleAppLog4Net/Program.cs
+++ ConsoleAppLog4Net/Program.cs
@@ -5,7 +5,7 @@ namespace ConsoleAppLog4Net
   class Program
   {
     private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-    private static void Main(string[] args)
+    private static void Main()
     {
       log.Info("This is my first logger example");
       log.Info("log info");
